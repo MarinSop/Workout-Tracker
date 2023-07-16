b885381a10aa3467bd7cefb8ba97a1211821c7e3
MarinSop <sopmarin.op@gmail.com>
2023-07-17 01:11:20 +0200

    JWT security

---
+++ restapi/src/main/java/com/ms/restapi/models/LoginRequest.java
@@ -0,0 +1,5 @@
+package com.ms.restapi.models;
+
+public record LoginRequest(String username, String password) {
+
+}
--- restapi/src/main/java/com/ms/restapi/security/SecurityConfig.java
+++ restapi/src/main/java/com/ms/restapi/security/SecurityConfig.java
@@ -1,14 +1,25 @@
 package com.ms.restapi.security;
 
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
 
 
 
@@ -19,19 +30,32 @@ public class SecurityConfig {
     @Autowired
     UserDetailsServiceImpl userDetailsService;
 
+    @Value("${jwt.key}")
+    private String jwtKey;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorizeRequests -> 
-        authorizeRequests
-        .requestMatchers("/login","/register","/logout").permitAll()
-        .anyRequest().authenticated())
-        .formLogin(Customizer.withDefaults())
+        authorizeRequests.anyRequest().authenticated())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
         .httpBasic(Customizer.withDefaults())
         .userDetailsService(userDetailsService) 
         .csrf(csrf -> csrf.disable());
 
         return http.build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(jwtKey.getBytes()));
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        byte[] bytes = jwtKey.getBytes();
+        SecretKeySpec originalKey = new SecretKeySpec(bytes, 0, bytes.length,"RSA");
+        return NimbusJwtDecoder.withSecretKey(originalKey).macAlgorithm(MacAlgorithm.HS512).build();
     }
 
     @Bean
